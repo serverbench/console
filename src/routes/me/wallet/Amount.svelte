10ebf834551bf18dcc6c7d421696f47a9ba4c93c
quiquelhappy <quiquelhappy@gmail.com>
2025-01-10 10:44:58 +0100

    fix amount format

--- src/routes/me/wallet/Amount.svelte
+++ src/routes/me/wallet/Amount.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
     import type Currency from "$lib/sb/store/Currency";
 
+    export let amount: number | null, currency: Currency | null;
+    let formattedAmount = "";
+    formatAmount(amount, currency);
 
-    export let amount: number, currency: Currency;
-    const formattedAmount = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: currency.code,
-    }).format(amount / (10 ** currency.digits));
+    $: amount, currency, formatAmount(amount, currency);
+    function formatAmount(amount: number | null, currency: Currency | null) {
+        if (amount == null || currency == null) {
+            return;
+        }
+        formattedAmount = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: currency.code,
+        }).format(amount / 10 ** currency.digits);
+    }
 </script>
 
 <span>
