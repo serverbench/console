9beb7fc519c867970fb392d5df5e468b0d2a944e
quiquelhappy <quiquelhappy@gmail.com>
2025-01-11 18:51:41 +0100

    remove chart

--- src/routes/community/members/+page.svelte
+++ src/routes/community/members/+page.svelte
@@ -1,25 +1,6 @@
 <script lang="ts">
-    import Candlestick from "$lib/components/sb/chart/Candlestick.svelte";
-    import Chart from "$lib/components/sb/chart/Chart.svelte";
     import Skeleton from "$lib/components/ui/skeleton/skeleton.svelte";
 
     let span: number = 60 * 24;
     let resolution: number = 15;
 </script>
-
-<Chart
-    height={300}
-    spans={{
-        "24h": [60 * 24, 15],
-        "7d": [60 * 24 * 7, 60],
-        "1mo": [60 * 24 * 7 * 30, 60 * 12],
-    }}
-    bind:span
-    bind:resolution
-    title="Online Players"
->
-    <Candlestick {resolution} {span} />
-</Chart>
-{#each Array(50) as _ }
-<Skeleton class="h-16 rounded"/>
-{/each}
